3c1545b065b65043680f8bbf42393b4390a0435c
wangye1221 <f_totti_ac@sina.com>
2016-05-10 19:27:07 +0800

    Create test/

---
+++ test/helloworld.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def SayHello(name):
+    print('%s你好' % name)
+    return
+name = input('请输入你的名字：')
+SayHello(name)
+
+#print(hex(255))
+
+#d = {'wangye':1221}
+#UserName = input('请输入用户名：')
+#n = 1
+#error = 1
+#while n == 1:
+#    if UserName in d:
+#        print('你的用户名是：%s, 密码是%s' % (UserName, d.get(UserName)))
+#        n = n + 1
+#    elif error < 3:
+#        print('你输入的用户名有误，请重新输入')
+#        error = error + 1
+#        UserName = input('请输入用户名：')
+#    else:
+#        print('用户名输入超过%i次，无法输入' % error)
+#        n = n + 1
+
+#L = ['Bart', 'Lisa', 'Adam']
+#for l in L:
+#    print("hello, %s!" % l)
+
+#print("**********BMI体重指数计算器**********")
+#height = float(input("请输入你的身高(米)："))
+#weight = float(input("请输入你的体重（千克）："))
+# = weight / (height * height)
+#if bmi < 18.5:
+#    print("你的BMI指数为：%.1f, 你的体重过轻"% bmi)
+#elif bmi >= 18.5 and bmi < 25:
+#    print("你的BMI指数为：%.1f, 你的体重正常"% bmi)
+#elif bmi >= 25 and bmi < 28:
+#    print("你的BMI指数为：%.1f, 你的体重过重"% bmi)
+#elif bmi >= 28 and bmi < 32:
+#    print("你的BMI指数为：%.1f, 你的体重肥胖"% bmi)
+#else:
+#    print("你的BMI指数为：%.1f, 你的体重严重肥胖"% bmi)
+#print("**********BMI体重指数计算器**********")
+
+#name = input('Please enter your name: ')
+#age = input("And enter your age: ")
+#print ('''hello, %s
+#your age: %s''' % (name, age))
+
+#print('''aaa
+#aaa
+#bbb
+#ccc''')
+
+#print("测试中文显示效果")
+
+#print("hello, %s, %s" % ("wangye", 124))
+
+#classmate = ["a", "b", "c"]
+#classmate.append("d")
+#print(classmate, len(classmate))
