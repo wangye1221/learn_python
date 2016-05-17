773b3c9b7ff80e8a5bd8f84e995aa95ec810adf1
wangye1221 <f_totti_ac@sina.com>
2016-05-17 15:09:20 +0800

    test

--- test/func_test.py
+++ test/func_test.py
@@ -19,5 +19,27 @@
 #print([x * x for x in range(1, 11)])
 #import os
 #print([d for d in os.listdir('/home/willwang')])
-L = ['Hello', 'World', 18, 'Apple', None]
-print([s.lower() for s in L if isinstance(s, str)])
+#L = ['Hello', 'World', 18, 'Apple', None]
+#print([s.lower() for s in L if isinstance(s, str)])
+
+# 生成器
+#g = (x for x in range(10))
+#for n in g:
+#    print(n)
+
+#斐波拉契数列和生成器
+#def fib(max):
+#    n, a, b = 0, 0, 1
+#    while n < max:
+##        print(b)
+#        yield b
+#        a, b = b, a + b
+#        n = n + 1
+#    return 'done'
+#for x in fib(10):
+#    print(x)
+
+#高阶函数
+def add(a, b, f):
+    return f(a) + f(b)
+print(add(4, -4, abs)) #将函数当作参数传入另一个函数中
