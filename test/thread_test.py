f82e278a26dacf1a9a81b6812b91ed261fbea51d
willwang <f_totti_ac@sina.com>
2016-05-23 15:42:00 +0800

    test

--- test/thread_test.py
+++ test/thread_test.py
@@ -5,3 +5,10 @@
 
 __author__ = 'Will Wang'
 
+import os
+print('Process (%s) start...' % os.getpid())
+pid = os.fork()
+if pid == 0:
+    print('这是子进程ID%s，父进程ID%s' % (os.getpid(), os.getppid()))
+else:
+    print('这是父进程ID%s，子进程ID%s' % (os.getpid(), pid))
