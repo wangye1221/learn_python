414461d5cf081337d64fbdc8c6f403df230aa0fe
willwang <f_totti_ac@sina.com>
2016-05-27 11:04:30 +0800

    rename thread_test to process_test

--- test/process_test.py
+++ test/process_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*-coding: utf-8 -*-
 
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from multiprocessing import Pool
 import os, time, random
 import subprocess
@@ -41,6 +41,28 @@ import subprocess
 #    p.join()
 #    print('All subprocesses done.')
 
-print('$ nslookup www.python.org')
-r = subprocess.call(['nslookup', 'www.python.org'])
-print('Exit code:',r)
+#print('$ nslookup www.python.org')
+#r = subprocess.call(['nslookup', 'www.python.org'])
+#print('Exit code:',r)
+
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+if __name__ == '__main__':
+    q = Queue()
+    pw = Process(target=write, args=(q,))
+    pr = Process(target=read, args=(q,))
+    pw.start()
+    pr.start()
+    pw.join()
+    pr.terminate()
