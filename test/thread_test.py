6c2553f7bfbfca241182a566317513aa22bbadb8
willwang <f_totti_ac@sina.com>
2016-05-27 16:09:09 +0800

    test

--- test/thread_test.py
+++ test/thread_test.py
@@ -3,6 +3,7 @@
 
 import time, threading
 
+'''
 def loop():
     print('thread %s is running...' % threading.current_thread().name)
     n = 0
@@ -17,3 +18,27 @@ t = threading.Thread(target=loop, name='LoopThread')
 t.start()
 t.join()
 print('thread %s ended.' % threading.current_thread().name)
+'''
+
+balance = 0
+lock = threading.Lock()
+def change_it(n):
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            lock.release()
+        
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
