0cb5bbd0d58af13a39ec5b169e49286db2e9afe6
willwang <f_totti_ac@sina.com>
2016-05-30 12:37:41 +0800

    test

--- test/regex_test.py
+++ test/regex_test.py
@@ -5,3 +5,9 @@ import re
 print(re.match(r'\d{3}\-\d{3,8}$', '010-12345'))
 print(re.match(r'^\d{3}\-\d{3,8}$', '010 12345'))
 print(re.split(r'\s+', 'a b     c             d'))
+
+m = re.match(r'^(\d{3})-(\d{3,8})$', '010-61610202')
+print(m.group())
+print(m.group(0))
+print(m.group(1))
+print(m.group(2))
